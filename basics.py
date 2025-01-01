c13d43142612cf370db019905aada9cff1b82ec2
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-01-01 12:33:32 +0800

    add basics.py with initial numpy array example

---
+++ basics.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# List
+a = np.array([1,2,3], dtype='int16') # dtype specify
+print(a)
+
+# 2d List
+b = np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])
+print(b)
+
+# Get Dimension
+print(a.ndim)
+print(b.ndim)
+
+# Get Shape
+print(a.shape)
+print(b.shape)
+
+# Get Type
+print(a.dtype)
+print(b.dtype)
+
+# Get Size
+print(a.itemsize)
+print(b.itemsize)
+
+# Get Total Size
+print(a.nbytes)
+print(b.nbytes)
