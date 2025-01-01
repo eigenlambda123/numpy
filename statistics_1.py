dd1873577639847e12a4d2a55f4e27a741356b6f
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-01-01 14:51:52 +0800

    Statistics

---
+++ statistics_1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+stats = np.array([[1,2,3], [4,5,6]])
+
+print(np.min(stats))
+print(np.max(stats))
+print(np.min(stats, axis=1))
+print(np.max(stats, axis=1))
+
+print(np.sum(stats))
+print(np.sum(stats, axis=1))
